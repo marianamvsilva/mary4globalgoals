88b098b071b10134a60729e1d5ce2be390ea542f
Mariana Silva <marianamvsilva@gmail.com>
2019-04-18 20:17:05 +0100

    Added date, time and temperature from API

--- weathify.js
+++ weathify.js
@@ -1,61 +1,76 @@
-navigator.geolocation.getCurrentPosition(function(position) {
-  let apiKey = "0d863e0f3f7819cc1dc71c86924341c5";
-  let apiUrl = "https://api.openweathermap.org/data/2.5";
-  let apiPath = `weather?lat=${position.coords.latitude}&lon=${
-    position.coords.longitude
-  }&appid=${apiKey}&units=metric`;
+let place = document.querySelector("#weather-city");
+let timestamp = document.querySelector("#timestamp");
+let description = document.querySelector("#weather-description");
+let temperature = document.querySelector("#weather-temperature");
+let humidity = document.querySelector("#weather-humidity");
+let windSpeed = document.querySelector("#weather-windspeed");
+let timeNow = document.querySelector("#time-now");
 
-  let h2 = document.querySelector("h2");
-  let getTemperature = document.querySelector(".getTemperature");
-  let sunriseHour = document.querySelector(".sunriseHour");
-  let sunsetHour = document.querySelector(".sunsetHour");
-  let currentHumidity = document.querySelector(".currentHumidity");
-  let currentWindSpeed = document.querySelector(".currentWindSpeed");
-  axios.get(`${apiUrl}/${apiPath}`).then(function(response) {
-    console.log(response.data);
-    h2.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
-    getTemperature.innerHTML = `${Math.round(response.data.main.temp)}º`;
-    sunriseHour.innerHTML = `Sunrise: ${response.data.sys.sunrise}`;
-    sunsetHour.innerHTML = `Sunset: ${response.data.sys.sunset}`;
-    currentHumidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
-    currentWindSpeed.innerHTML = `Wind: ${response.data.wind.speed} km/H`;
-  });
-});
+place.innerHTML = "Lisbon";
+timestamp.innerHTML = "Thursday, 7:00 PM";
+description.innerHTML = "Sunny";
+humidity.innerHTML = 19999;
+windSpeed.innerHTML = 133;
 
-let date = new Date();
 function formatDate(date) {
-  let weekDays = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednsday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
+  let weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   let monthNames = [
-    "January",
-    "February",
-    "Marc",
-    "April",
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
     "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
   ];
+
   let day = weekDays[date.getDay()];
   let month = monthNames[date.getMonth()];
+
+  return `${day}, ${month} ${date.getDate()} ${date.getFullYear()}`;
+}
+
+let now = new Date();
+let currentDate = document.querySelector("#timestamp");
+currentDate.innerHTML = formatDate(now);
+
+function formatTime(date) {
+  let hours = date.getHours();
   let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  let currentDate = document.querySelector(".currentDate");
-  let currentHour = document.querySelector(".currentHour");
-  currentDate.innerHTML = `${day}, ${month} ${date.getDate()}, ${date.getFullYear()}`;
-  currentHour.innerHTML = `${date.getHours()}h${minutes}`;
+
+  return `${hours}:${minutes}`;
 }
-formatDate(date);
+timeNow.innerHTML = formatTime(now);
+
+let apiKey = "020587e6f2a6601e01854941fcf9435b";
+let apiUrl = "https://api.openweathermap.org/data/2.5";
+let apiPath = "weather";
+let city = "Lisbon";
+let apiParams = `q=${city}&appid=${apiKey}&units=metric`;
+
+function handleResponse(response) {
+  console.log(response.data);
+  let temperature = Math.round(response.data.main.temp);
+  console.log(temperature);
+  let weatherTemperature = document.querySelector("#weather-temperature");
+  weatherTemperature.innerHTML = `${Math.round(temperature)}`;
+}
+
+axios.get(`${apiUrl}/${apiPath}?${apiParams}`).then(handleResponse);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  alert(cityInput.value);
+}
+
+let form = document.querySelector("#city-form");
+form.addEventListener("submit", handleSubmit);
